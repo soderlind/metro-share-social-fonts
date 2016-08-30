5261c0a4233532e863117edcd103c35b37a23a9f
soderlind <per@soderlind.no>
2016-08-30 18:52:55 +0200

    Rename files

--- metro-share-social-fonts.php
+++ metro-share-social-fonts.php
@@ -3,14 +3,14 @@
 Plugin Name: Metro Share Social Fonts
 Plugin URI: http://soderlind.no/
 Description: Replace the default Metro Share social icons with icomoon social fonts
-Version: 0.1
+Version: 0.2.0
 Author: Per Soderlind
 Author URI: http://soderlind.no/
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
-
+define( 'METROSHARESOCIALFONTS_VERSION', '0.2.0' );
 /*
  * Dequeue Metroshare CSS file
  *
@@ -29,7 +29,6 @@ add_action( 'wp_enqueue_scripts', 'metroshare_remove_default_styles', 11 );
  * @author Per Soderlind <per@soderlind.no>
  */
 function metroshare_add_social_fonts() {
-	wp_enqueue_style( 'metroshare-social-fonts', plugins_url( '/css/social.min.css?v=0.1', __FILE__ ) );
+	wp_enqueue_style( 'metroshare-social-fonts', plugins_url( '/css/social.min.css', __FILE__ ), array() , METROSHARESOCIALFONTS_VERSION );
 }
 add_action( 'wp_enqueue_scripts', 'metroshare_add_social_fonts' );
-
